87166501d98ee9e66eac264243d2006df6243903
Dmitriy <john.smith.dm8338@gmail.com>
2025-01-29 20:01:59 +0200

    - clean angular.json for zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideAnimationsAsync(),
     provideExperimentalZonelessChangeDetection(),
   ]
