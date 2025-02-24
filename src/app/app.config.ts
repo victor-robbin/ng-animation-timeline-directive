766d6c80658af1a6cbd4fcecfccb452cc76bd372
Dmitriy <john.smith.dm8338@gmail.com>
2025-02-24 17:19:04 +0200

    - new features for Angular 18 in route proider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    }), withComponentInputBinding()),
     provideAnimationsAsync(),
     provideExperimentalZonelessChangeDetection(),
   ]
